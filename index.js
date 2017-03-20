cc9e0d52a3ef7c7e515e7929a2f7eef5e6e9925e
Unknown <javiermartincuenca@gmail.com>
2017-03-20 14:05:36 +0100

    Agregado express para el servidor
    Tambien se han bloqueado los comando ls y cd

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-var http = require('http'),
+/*var http = require('http'),
     io = require('socket.io'),
-    fs = require('fs'),
-    spawn = require('child_process').spawn;
+    fs = require('fs'),*/
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var spawn = require('child_process').spawn;
 
 //Puerto al que se conecta el servidor
-var puerto = 9090;
+const puerto = 9090;
 
 //Tiempo de expiracion de un comando (en milisegundos)
 const timer_ms = 5000;
 
-var app = http.createServer(function(req, res) {
+app.use(express.static('client'));
+
+server.listen(puerto, function() {
+    console.log('Servidor funcionando en http://localhost:' + puerto);
+});
+
+
+
+/*var app = http.createServer(function(req, res) {
 
     console.log('Servidor funcionando en http://localhost:' + puerto);
 
@@ -26,16 +39,69 @@ var app = http.createServer(function(req, res) {
             });
         }
     })
-});
+});*/
 
-app.listen(puerto);
-
+//app.listen(puerto);
+/*
 io.listen(app).sockets.on('connection', function(socket) {
 
     console.log('Cliente conectado');
 
     var shell = spawn('/usr/bin/maude'),
         stdin = shell.stdin;
+    var temporizador;
+
+    shell.on('exit', function() {
+        socket.disconnect();
+    })
+
+    shell['stdout'].setEncoding('ascii');
+    shell['stdout'].on('data', function(data) {
+        clearTimeout(temporizador);
+        socket.emit('stdout', data);
+
+    });
+
+    shell['stderr'].setEncoding('ascii');
+    shell['stderr'].on('data', function(data) {
+        clearTimeout(temporizador);
+        socket.emit('stderr', data);
+
+    });
+
+    socket.on('stdin', function(command) {
+        stdin.write(command + "\n") || socket.emit('disable');
+
+        temporizador = setTimeout(matar_maude, timer_ms);
+    });
+
+    stdin.on('drain', function() {
+        socket.emit('enable');
+    });
+
+    stdin.on('error', function(exception) {
+        socket.emit('error', String(exception));
+    });
+
+    socket.on('disconnect', matar_maude);
+
+    function matar_maude() {
+        console.log("salida por timeout");
+        try {
+            process.kill(shell.pid, 'SIGKILL');
+        } catch (ex) {
+            console.log(ex);
+        }
+
+        socket.disconnect();
+    }
+});*/
+
+io.on('connection', function(socket) {
+    console.log('Cliente conectado');
+    var shell = spawn('/bin/bash');
+    //var shell = spawn('/usr/bin/maude');
+    var stdin = shell.stdin;
     var temporizador;
 
     shell.on('exit', function() {
