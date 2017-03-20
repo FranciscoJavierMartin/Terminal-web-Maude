9919e154b31e3c2737fde1b24f73c25fa03b9b59
Unknown <javiermartincuenca@gmail.com>
2017-03-20 01:52:06 +0100

    Se ha agregado un temporizador
    Agregado un temporizador para evitar la ejecucion de comandos infinitos

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var http = require('http'),
     spawn = require('child_process').spawn;
 
 var puerto = 9090;
+//Tiempo de expiracion de un comando
+const timer_ms = 5000;
 
 var app = http.createServer(function(req, res) {
+
+    console.log('Servidor funcionando en http://localhost:' + puerto);
+
     [
         '/index.html', '/js/jquery.terminal.min.js', '/css/jquery.terminal.css', '/css/estilo.css'
     ].forEach(function(file) {
@@ -24,8 +29,12 @@ var app = http.createServer(function(req, res) {
 app.listen(puerto);
 
 io.listen(app).sockets.on('connection', function(socket) {
+
+    console.log('Cliente conectado');
+
     var shell = spawn('/usr/bin/maude'),
         stdin = shell.stdin;
+    var temporizador;
 
     shell.on('exit', function() {
         socket.disconnect();
@@ -33,16 +42,31 @@ io.listen(app).sockets.on('connection', function(socket) {
 
     shell['stdout'].setEncoding('ascii');
     shell['stdout'].on('data', function(data) {
+        clearTimeout(temporizador);
         socket.emit('stdout', data);
+
     });
 
     shell['stderr'].setEncoding('ascii');
     shell['stderr'].on('data', function(data) {
+        clearTimeout(temporizador);
         socket.emit('stderr', data);
+
     });
 
     socket.on('stdin', function(command) {
         stdin.write(command + "\n") || socket.emit('disable');
+
+        temporizador = setTimeout(function() {
+            console.log("salida por timeout");
+            try {
+                process.kill(shell.pid, 'SIGKILL');
+            } catch (ex) {
+                console.log(ex);
+            }
+
+            socket.disconnect();
+        }, timer_ms);
     });
 
     stdin.on('drain', function() {
